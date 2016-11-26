ea5d9f7f274aec8ffa9a7abd6bc3fde9fcfddba7
ameurm <ameur.mezaache@gmail.com>
2016-11-25 21:35:11 -0500

    Add files via upload

---
+++ GizmoProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjections
+{
+    class GizmoProcessor
+    {
+        //public void Process()
+        //{
+        //    // do something
+        // //   var logger = new TextLogger();
+        //   // logger.WriteLogMessage("Something happened");
+        //}
+
+        //Constructor Injection 
+        private readonly ILogger _logger;
+        public GizmoProcessor(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Process()
+        {
+            // do something
+            _logger.WriteLogMessage("Something happened");
+        }
+
+
+    }
+    public class TextLogger : ILogger
+    {
+        public void WriteLogMessage(string Message)
+        {
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var processor = new GizmoProcessor();
+            //processor.Process();
+            var processor = new GizmoProcessor(new TextLogger());
+        }
+    }
+}
